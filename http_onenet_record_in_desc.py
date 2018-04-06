bfdbc8fe64018705243e8c2a9c3141eea708de48
cohung_mac <cohung@163.com>
2018-04-06 21:58:21 +0800

    假设短信信息参数存储在title为mysmsrec的设备中，记录数据的字段同记录领用记录的字段一样，仍为desc。

--- http_onenet_record_in_desc.py
+++ http_onenet_record_in_desc.py
@@ -22,7 +22,9 @@ request_num=0 #全局变量，用于记录扫描次数。不参与具体事务
 
 while True:
 		reply="" #作用：存储每次查询记录的初始返回数据。新的扫描开始前需要清空。
+		reply_sms="" #作用：存储每次查询短信提示的初始返回数据。新的扫描开始前需要清空。
 		rec_id=[] #作用：用来记录所有传感器ID用于后续删除。新的扫描开始前需要清空。
+		sms_id=[] #作用：用来记录所有传感器ID用于后续删除。新的扫描开始前需要清空。
 		request_num=request_num+1
 		print "这是第"+str(request_num)+"次查询！"
 
@@ -49,12 +51,12 @@ while True:
 
 		#print ('Socket Connected to '+host+' on IP '+remote_ip)
 		#handling errors in python socket programs
-
-		message = "GET /devices?key_words=mydevice HTTP/1.1\r\nHost:api.heclouds.com\r\nAccept:*/*\r\napi-key:X6gNMI=4KPqDDLuHNLR40rS=9nY=\r\nContent-Length:0\r\nConnection:close\r\n\r\n"
+		###################以下先读取领用记录信息
+		message = "GET /devices?key_words=mydevice HTTP/1.1\r\nHost:api.heclouds.com\r\nAccept:*/*\r\napi-key:X6gNMI=4KPqDDLuHNLR40rS=9nY=\r\nContent-Length:0\r\n\r\n"
 		try:
 			#Set the whole string
 			s.sendall(message.encode("UTF-8"))
-			#print(message.encode("UTF-8"))
+			print(message.encode("UTF-8"))
 		except socket.error:
 			#Send Failed
 			print 'Send Failed'
@@ -63,27 +65,48 @@ while True:
 		#print ('Message send successfully')
 
 		#Now receive data
-		while True:
-			recv_data=s.recv(8192)
-			if not recv_data: break
-			reply+=recv_data
-		#reply = s.recv(8192) #8192这个缓冲区的大小直接影响到接收数据的完整程度
-		
+		# while True:
+			# print "in recv"
+			# recv_data=s.recv(8192)
+			# if not recv_data: break
+			# reply+=recv_data
+		reply = s.recv(8192) #8192这个缓冲区的大小直接影响到接收数据的完整程度
+		#print "out recv"
+		###################以下再读取短信记录信息
+		message = "GET /devices?key_words=mysmsrec HTTP/1.1\r\nHost:api.heclouds.com\r\nAccept:*/*\r\napi-key:X6gNMI=4KPqDDLuHNLR40rS=9nY=\r\nContent-Length:0\r\n\r\n"
+		try:
+			#Set the whole string
+			s.sendall(message.encode("UTF-8"))
+			print(message.encode("UTF-8"))
+		except socket.error:
+			#Send Failed
+			print 'Send Failed'
+			sys.exit()
+			
+		#print ('Message send successfully')
+
+		#Now receive data
+		# while True:
+			# print "in recv"
+			# recv_data=s.recv(8192)
+			# if not recv_data: break
+			# reply+=recv_data
+		reply_sms = s.recv(8192) #8192这个缓冲区的大小直接影响到接收数据的完整程度
+		#print "out recv"
+		####################以下断开扫描领用记录和短信通知
 		s.close()
 		
-		#f=open("./pre.txt","a") #追加写入
-		#f.write(reply.encode('utf-8')) #rec_str是自创的字符串，默认为ascii编码
-		#f.close()
-		
+		# f=open("./pre.txt","a") #追加写入
+		# f.write(reply_sms.encode('utf-8')) #rec_str是自创的字符串，默认为ascii编码
+		# f.close()
+		####################以下处理收集到的领用记录数据
 		str_num = reply.find('{')
 		reply_1 = reply[str_num:] #首先完成将接收到的带HTTP头的返回数据的清理--去除HTTP头		
 		
 		reply_2 = json.loads(reply_1) #将返回数据转换为python对象
 
 		rec_str = "" #用于存储领用记录。每次扫描开始前需要清空。
-		sms_str = [] #用于存储短信发送目标,每条发送目标是一条字符串，作为元素存入列表
 		num = reply_2["data"]["total_count"] #num为此次扫描返回的记录数量
-		sms_num = 0 #记录后续需要发送短信的数量
 
 		#print reply_2["data"]["total_count"]#此处输出为返回的记录数据条数，用于后续的循环输出
 		for rec_num in range(0,num):
@@ -93,7 +116,22 @@ while True:
 		f=open("./http_onenet_record_in_desc.txt","a") #追加写入
 		f.write(rec_str.encode('utf-8')) #rec_str是自创的字符串，默认为ascii编码
 		f.close()
-		
+		####################以下处理收集到的短信记录数据
+		str_num = reply_sms.find('{')
+		reply_1 = reply_sms[str_num:] #首先完成将接收到的带HTTP头的返回数据的清理--去除HTTP头				
+		reply_2 = json.loads(reply_1) #将返回数据转换为python对象
+
+		sms_str = "" #用于存储短信发送目标,每条发送目标是一条字符串，作为元素存入列表
+		sms_num = reply_2["data"]["total_count"] #num为此次扫描返回的记录数量
+
+		#print reply_2["data"]["total_count"]#此处输出为返回的记录数据条数，用于后续的循环输出
+		for rec_num in range(0,sms_num):
+			sms_str = sms_str + reply_2["data"]["devices"][rec_num]["desc"] + "\r\n"
+			sms_id.append(reply_2["data"]["devices"][rec_num]["id"])			
+
+		f=open("./http_onenet_sms_in_desc.txt","a") #追加写入
+		f.write(sms_str.encode('utf-8')) #rec_str是自创的字符串，默认为ascii编码
+		f.close()
 		
 		##########得到所需记录后删除Onenet上的存储##############
 		
